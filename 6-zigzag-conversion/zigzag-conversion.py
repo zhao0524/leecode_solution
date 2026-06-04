ba21ece6ffa34efb7da4874d6c528c185701b183
David Zhao <115097081+zhao0524@users.noreply.github.com>
2026-06-03 20:06:08 -0400

    Time: 7 ms (86.05%) | Memory: 19.4 MB (21.38%) - LeetSync

--- 6-zigzag-conversion/zigzag-conversion.py
+++ 6-zigzag-conversion/zigzag-conversion.py
@@ -7,7 +7,6 @@ class Solution:
         result = [""] * numRows
         b = 1
         k = 0
-        final = ""
 
         for i in s:
             result[k] = result[k]+i 
@@ -19,10 +18,7 @@ class Solution:
             elif k == numRows-1:
                 b = -1
         
-        for i in range(numRows):
-            final += result[i]
-        
-        return final
+        return "".join(result)
 
         
 
